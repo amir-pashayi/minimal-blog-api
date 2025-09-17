5427a486848610fc218452a46f7aa7d833228c22
Amir Pashayi <amir.pasha.dev@gmail.com>
2025-09-17 17:50:22 +0330

    configure admin panel and fix small bugs

--- accounts/managers.py
+++ accounts/managers.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, phone, name, age, **extra_fields):
+    def create_user(self, phone, name, age, password=None, **extra_fields):
         if not phone:
             raise ValueError('phone is required')
 
         user = self.model(phone=phone, name=name, age=age, **extra_fields)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
         user.save(using=self._db)
-        return user
 
     def create_superuser(self, phone, name, age, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
