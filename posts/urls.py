d52b4a055ed728bd8f7bfdb39ed558583c80d068
Amir Pashayi <amir.pasha.dev@gmail.com>
2025-09-19 14:27:55 +0330

    add post like endpoint

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("my-posts/", views.MyPostsListAPIView.as_view(), name="my-posts"),
     path("", views.PostListCreateAPIView.as_view(), name="post-list-create"),
     path("<slug:slug>/", views.PostDetailAPIView.as_view(), name="post-detail"),
+    path("posts/<slug:slug>/like/", views.LikePostView.as_view(), name="post-like"),
     path('author/<str:username>/', views.AuthorPostsAPIView.as_view(), name='author-posts'),
 
 ]
