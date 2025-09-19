53434ff3e7246bc8e64604297b21c97922476bdd
Amir Pashayi <amir.pasha.dev@gmail.com>
2025-09-19 09:10:27 +0330

    add .env for secret key, debug and hosts

--- blog/settings.py
+++ blog/settings.py
@@ -12,21 +12,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "change-me-in-dev-only")
+DEBUG = os.getenv("DJANGO_DEBUG", "1") in ("1", "true", "True", "YES", "yes")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f_yd*z=q%vm3%e_p14w_!m19x&x0y05vhuh1$4^m*)4cmx0q&&'
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
